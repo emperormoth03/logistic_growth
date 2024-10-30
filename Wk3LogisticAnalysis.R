5fbbeda641e973fa89fc7aa47b6da5954a0ed7e4
emperormoth03 <william.withers@sjc.ox.ac.uk>
2024-10-30 12:35:09 +0000

    Please

---
+++ Wk3LogisticAnalysis.R
@@ -0,0 +1,96 @@
+growth_data <- read.csv("experiment.csv")
+
+install.packages("ggplot2")
+library(ggplot2)
+
+ggplot(aes(t,N), data = growth_data) +
+  
+  geom_point() +
+  
+  xlab("t") +
+  
+  ylab("y") +
+  
+  theme_bw()
+
+ggplot(aes(t,N), data = growth_data) +
+  
+  geom_point() +
+  
+  xlab("t") +
+  
+  ylab("y") +
+  
+  scale_y_continuous(trans='log10')
+
+
+
+
+
+#Script to estimate the model parameters using a linear approximation
+library(dplyr)
+
+growth_data <- read.csv("experiment.csv")
+
+#Case 1. K >> N0, t is small
+
+data_subset1 <- growth_data %>% filter(t<1000) %>% mutate(N_log = log(N))
+
+model1 <- lm(N_log ~ t, data_subset1)
+summary(model1)
+
+
+
+#Case 2. N(t) = K
+
+data_subset2 <- growth_data %>% filter(t>3000)
+
+model2 <- lm(N ~ 1, data_subset2)
+summary(model2)
+
+
+
+
+
+
+
+
+
+
+
+
+growth_data <- read.csv("experiment.csv")
+
+logistic_fun <- function(t) {
+  
+  N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
+  
+  return(N)
+  
+}
+
+  N0 <- 879 #
+  
+  r <- 0.01 #
+  
+  K <- 6e+10 #
+  
+  ggplot(aes(t,N), data = growth_data) +
+  
+  geom_function(fun=logistic_fun, colour="red") +
+  
+  geom_point() +
+
+  scale_y_continuous(trans='log10')
+  
+  fsdfdf
+
+
+  
+  
+  sink(file = "package-versions.txt")
+  sessionInfo()
+  sink()
+  
+  
+
--- fit_linear_model.R
+++ fit_linear_model.R
@@ -2,18 +2,18 @@
 
 #library(dplyr)
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment.csv")
 
 #Case 1. K >> N0, t is small
 
-data_subset1 <- growth_data %>% filter(t<???) %>% mutate(N_log = log(N))
+data_subset1 <- growth_data %>% filter(t<N) %>% mutate(N_log = log(N))
 
 model1 <- lm(N_log ~ t, data_subset1)
 summary(model1)
 
 #Case 2. N(t) = K
 
-data_subset2 <- growth_data %>% filter(t>???)
+data_subset2 <- growth_data %>% filter(t>N)
 
 model2 <- lm(N ~ 1, data_subset2)
 summary(model2)
